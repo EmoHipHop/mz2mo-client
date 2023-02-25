36dfdfee60e2356419a2ad1648e97801ad2087be
RookieAND <gwangin1999@naver.com>
2023-02-25 22:41:15 +0900

    fix: fix mismatch between Next and React

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,13 @@ import Head from 'next/head';
 
 const Home = () => {
   return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#00917C" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>지식의 요람, KU : AGORA</title>
-      </Head>
-    </>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#00917C" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>지식의 요람, KU : AGORA</title>
+    </Head>
   );
 };
 

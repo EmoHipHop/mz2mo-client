d762d9592ba6a9933937a4ea06f51b701e0aac6f
leeharyung <lhr4884@naver.com>
2023-02-26 16:51:40 +0900

    comment: add comment

--- src/hooks/query/spotify/player/useStartAndPausePlaybackMutation.tsx
+++ src/hooks/query/spotify/player/useStartAndPausePlaybackMutation.tsx
@@ -4,7 +4,7 @@ import { putAsync } from '@/apis/API';
 import { START_AND_RESUME } from '@/constants/apis/spotify';
 import { SPOTIFY_API_URL } from '@/constants/apis/server';
 
-interface UseStartAndPausePlaybackMutationProps {
+interface UseStartAndResumePlaybackMutationProps {
   deviceId: string;
   contextUri?: string;
   uris?: string[];
@@ -12,13 +12,22 @@ interface UseStartAndPausePlaybackMutationProps {
   positionMs?: number;
 }
 
-const useStartAndPausePlaybackMutation = ({
+/**
+ * 재생을 시작하거나 재개하는 Hook
+ * @param deviceId 기기 id
+ * @param contextUri 재생할 컨텍스트 uri
+ * @param uris 재생할 트랙 uri
+ * @param offset 재생할 트랙의 오프셋
+ * @param positionMs 재생할 트랙의 시작 위치
+ * @returns useMutation
+ */
+const useStartAndResumePlaybackMutation = ({
   deviceId,
   contextUri,
   uris,
   offset,
   positionMs,
-}: UseStartAndPausePlaybackMutationProps) => {
+}: UseStartAndResumePlaybackMutationProps) => {
   return useMutation([START_AND_RESUME], () =>
     putAsync(`${SPOTIFY_API_URL}${START_AND_RESUME}`, {
       headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }, // TODO: fix get token
@@ -35,4 +44,4 @@ const useStartAndPausePlaybackMutation = ({
   );
 };
 
-export default useStartAndPausePlaybackMutation;
+export default useStartAndResumePlaybackMutation;

2372b2679dd07461cc5bdcb069b0778519ef0834
leeharyung <lhr4884@naver.com>
2023-02-26 17:05:39 +0900

    feat: add pausePlayback hook

--- src/constants/apis/spotify/index.ts
+++ src/constants/apis/spotify/index.ts
@@ -3,3 +3,4 @@ export const TRACKS = '/tracks';
 export const PLAYER = '/me/player';
 export const CURRENTLY_PLAYING = `${PLAYER}/currently-playing`;
 export const START_AND_RESUME = `${PLAYER}/play`;
+export const PAUSE = `${PLAYER}/pause`;

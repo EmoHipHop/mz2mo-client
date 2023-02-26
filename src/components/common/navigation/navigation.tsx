55bd68c229a5e46b0f724882c0ad0d8d2d2645af
moo_miz <moo_miz@naver.com>
2023-02-27 01:44:42 +0900

    feat:navbar publishing

--- src/components/common/navigation/navigation.tsx
+++ src/components/common/navigation/navigation.tsx
@@ -1,31 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { theme } from '@/constants/styles/theme';
 import Image from 'next/image';
-import Menu from 'public/assets/icons/menu.svg';
-import Logo from 'public/assets/icons/logo.svg';
 
 const NavBody = styled.div`
   width: 100%;
-  height: 65px;
   left: 0;
   position: fixed;
   display: flex;
   align-items: center;
-  justify-content: space-between;
   line-height: 65px;
   text-align: center;
+  justify-content: space-between;
   background-color: black;
 `;
 
-const Container = styled.div`
-  /* @media screen and (min-width: 768px) and (max-width: 1024px) {
-    margin: 0;
-  } */
-`;
-
 const LogoArea = styled.div`
-  border: red solid 1px;
+  width: 200px;
+  justify-content: center;
+  padding-left: 3rem;
 `;
 
 const MenuList = styled.div`
@@ -55,60 +48,46 @@ const MenuText = styled.a`
   text-decoration: none;
   color: #7e7d85;
   line-height: 65px;
-  font-size: 2.4rem;
+  font-size: 1.2rem;
 `;
-
-const NavTrigger = styled.div`
-  cursor: pointer;
-  width: 30px;
-  height: 25px;
-  margin: auto;
-  position: absolute;
-  right: 30px;
-  top: 0;
-  bottom: 0;
+const Hambuger = styled.div`
+  padding-top: 8px;
+  color: #7e7d85;
+  width: 48px;
+  height: 48px;
 `;
-
 const Navigation = () => {
-  const menuToggle = useRef<HTMLButtonElement>(null);
-
-  const toggleClick = () => {
-    menuToggle.current?.classList.toggle('active');
-  };
-
   return (
     <NavBody>
-      <Container>
-        <LogoArea>
-          <Image
-            src="/assets/icons/logo.svg"
-            width={159}
-            height={25}
-            layout="fill"
-          />
-        </LogoArea>
-        <MenuList>
-          <MenuLinks>
-            <MenuItem>
-              <MenuText>가입하기</MenuText>
-            </MenuItem>
-            <MenuItem>
-              <MenuText>|</MenuText>
-            </MenuItem>
-            <MenuItem>
-              <MenuText>로그인</MenuText>
-            </MenuItem>
-          </MenuLinks>
-          <span>
+      <LogoArea>
+        <Image
+          src="/assets/icons/logo.svg"
+          width={153}
+          height={40}
+          layout="responsive"
+        />
+      </LogoArea>
+      <MenuList>
+        <MenuLinks>
+          <MenuItem>
+            <MenuText>가입하기</MenuText>
+          </MenuItem>
+          <MenuItem>
+            <MenuText>|</MenuText>
+          </MenuItem>
+          <MenuItem>
+            <MenuText>로그인</MenuText>
+          </MenuItem>
+          <Hambuger>
             <Image
               src="/assets/icons/menu.svg"
-              width={40}
-              height={40}
-              layout="fill"
+              width={30}
+              height={30}
+              layout="responsive"
             />
-          </span>
-        </MenuList>
-      </Container>
+          </Hambuger>
+        </MenuLinks>
+      </MenuList>
     </NavBody>
   );
 };

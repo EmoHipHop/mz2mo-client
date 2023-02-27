13f40e5ff1fb6c7d21e9fab516347427151ed2e5
RookieAND <gwangin1999@naver.com>
2023-02-27 17:31:54 +0900

    feat: connect feature of SDK in Webplayer component
    Webplayer 컴포넌트 내에 SDK와 상호 작용 가능한 기능들의 연동을 완료했습니다.

--- global.d.ts
+++ global.d.ts
@@ -9,3 +9,5 @@ declare module '*.svg' {
   const svg: React.FC<React.SVGProps<SVGSVGElement>>;
   export default svg;
 }
+
+export {};
--- src/components/common/web-players/WebPlayers.tsx
+++ src/components/common/web-players/WebPlayers.tsx
@@ -1,26 +1,33 @@
 import { useEffect, useState } from 'react';
 import { useAtomValue } from 'jotai';
 
-import { sdkPlayerPlayTrack, sdkPlayerPauseTrack } from '@/utils/spotify';
 import { setSpotifyTokenAtom } from '@/stores/actions';
-
-interface WebPlaybackPlayer {
-  device_id: string;
-}
+import { SdkStatusTypes } from '@/types/spotifyTypes';
+import {
+  sdkPlayerPlayTrack,
+  sdkPlayerPauseTrack,
+  sdkPlayerSetRepeat,
+} from '@/utils/spotify';
 
 const WebPlayers = () => {
-  const [spotifyPlayer, setSpotifyPlayer] = useState<
-    typeof window.Spotify.Player | null
-  >(null);
   const [deviceId, setDeviceId] = useState('');
+  const [sdkPlayer, setSdkPlayer] = useState<Spotify.Player | null>(null);
+  const [currentTrack, setCurrentTrack] = useState<Spotify.Track | null>(null);
+  const [sdkStatus, setSdkStatus] = useState<SdkStatusTypes>({
+    paused: false,
+    actived: false,
+    repeated: 'off',
+    position: 0,
+  });
   const spotifyToken = useAtomValue(setSpotifyTokenAtom);
 
-  const onPlay = async () => {
+  const onPlay = () => {
     if (spotifyToken)
       sdkPlayerPlayTrack(
         ['spotify:track:7eBpUuPnDTfbeP1P4P93CS'],
         deviceId,
         spotifyToken,
+        sdkStatus.position,
       );
   };
 
@@ -28,41 +35,92 @@ const WebPlayers = () => {
     if (spotifyToken) sdkPlayerPauseTrack(deviceId, spotifyToken);
   };
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://sdk.scdn.co/spotify-player.js';
-    script.async = true;
-    document.body.appendChild(script);
+  const onRepeat = () => {
+    if (spotifyToken) {
+      const changedRepeated = sdkStatus.repeated === 'off' ? 'track' : 'off';
+      sdkPlayerSetRepeat(deviceId, spotifyToken, changedRepeated);
+      setSdkStatus((prevStatus) => ({
+        ...prevStatus,
+        repeated: changedRepeated,
+      }));
+    }
+  };
 
-    const connectToSpotify = async (token: string) => {
+  useEffect(() => {
+    // SpotifyToken 이 존재하지 않는다면, SDK 연결이 불가하므로 script 로드 미진행.
+    if (spotifyToken !== null) {
+      const script = document.createElement('script');
+      script.src = 'https://sdk.scdn.co/spotify-player.js';
+      script.async = true;
+      document.body.appendChild(script);
+
       window.onSpotifyWebPlaybackSDKReady = () => {
-        const player = new window.Spotify.Player({
+        // Spotify 로부터 새로운 SDK Player 객체 생성
+        const initPlayer = new window.Spotify.Player({
           name: 'Web Playback SDK',
-          getOAuthToken: (callback: (t: string) => void) => {
-            callback(token);
+          getOAuthToken: (callback) => {
+            callback(spotifyToken);
           },
           volume: 0.5,
         });
-        setSpotifyPlayer(player);
-        player.addListener('ready', (webPlaybackPlayer: WebPlaybackPlayer) => {
-          setDeviceId(webPlaybackPlayer.device_id);
+
+        // Spotify.Player 객체를 ref 로 저장
+        setSdkPlayer(initPlayer);
+
+        // SDK 세팅이 완료되었다면 인계 받은 deviceId 저장
+        initPlayer.addListener('ready', (data) => {
+          setDeviceId(data.device_id);
+          console.log(`Ready with Device ID : ${data.device_id}`);
         });
 
-        player.setVolume(0.5);
-        player.connect();
-      };
-    };
+        initPlayer.addListener('not_ready', (data) => {
+          console.log(`Device ID : ${data.device_id} has gone Offline.`);
+        });
 
-    if (spotifyToken !== null) {
-      connectToSpotify(spotifyToken);
+        initPlayer.addListener('player_state_changed', (state) => {
+          if (!state) return;
+
+          // 현재 재생된 음악의 트랙 정보에 대한 정보를 받아옴.
+          setCurrentTrack(state.track_window.current_track);
+
+          // Player 재생 중지 시 중지된 position 을 저장하여 resume에 사용
+          setSdkStatus((prevStatus) => ({
+            ...prevStatus,
+            paused: state.paused,
+            position: state.paused ? state.position : 0,
+          }));
+
+          initPlayer.getCurrentState().then((curState) => {
+            setSdkStatus((prevStatus) => ({
+              ...prevStatus,
+              active: !(curState === null),
+            }));
+          });
+        });
+
+        // 사용자의 기본 볼륨을 50%로 설정한 후 SDK 연결 진행
+        initPlayer.setVolume(0.5);
+        initPlayer.connect();
+      };
     }
-  }, [spotifyToken]);
+  }, []);
 
   return (
     <div>
       <p>SDK Player</p>
-      <button onClick={onPlay}>play Song</button>
-      <button onClick={onPause}>pause Song</button>
+      {sdkPlayer && (
+        <div>
+          {currentTrack && (
+            <div>
+              <p>{`${currentTrack.name}`}</p>
+              <p>{`${currentTrack.artists[0].name}`}</p>
+            </div>
+          )}
+          <button onClick={onPlay}>play Song</button>
+          <button onClick={onPause}>pause Song</button>
+          <button onClick={onRepeat}>Repear Song</button>
+        </div>
+      )}
     </div>
   );
 };

eaee0217d1dc08b6ab005a43d59456486314487e
RookieAND <gwangin1999@naver.com>
2023-02-27 21:24:35 +0900

    feat: modify navigation components
    네비게이션 임시 로그인 기능을 추가하고, 관련 기능을 수정하였습니다.

--- src/components/common/navigation/navigation.tsx
+++ src/components/common/navigation/navigation.tsx
@@ -1,93 +1,41 @@
-import React, { useRef, useState } from 'react';
-import styled from 'styled-components';
 import Image from 'next/image';
 
-const NavBody = styled.div`
-  width: 100%;
-  left: 0;
-  position: fixed;
-  display: flex;
-  align-items: center;
-  line-height: 65px;
-  text-align: center;
-  justify-content: space-between;
-  background-color: black;
-`;
+import { redirectToSpotifyAuth } from '@/apis/spotify';
+import * as style from './navigation.style';
 
-const LogoArea = styled.div`
-  width: 200px;
-  justify-content: center;
-  padding-left: 3rem;
-`;
-
-const MenuList = styled.div`
-  height: inherit;
-  display: flex;
-  align-items: center;
-  padding-right: 3rem;
-  padding-left: 1rem;
-`;
-
-const MenuLinks = styled.ul`
-  width: 100%;
-  height: 65px;
-  display: flex;
-  list-style: none;
-  margin: 0;
-`;
-
-const MenuItem = styled.li`
-  width: auto;
-  height: 65px;
-  padding: 0;
-  padding-right: 3rem;
-`;
-
-const MenuText = styled.a`
-  text-decoration: none;
-  color: #7e7d85;
-  line-height: 65px;
-  font-size: 1.2rem;
-`;
-const Hambuger = styled.div`
-  padding-top: 8px;
-  color: #7e7d85;
-  width: 48px;
-  height: 48px;
-`;
 const Navigation = () => {
   return (
-    <NavBody>
-      <LogoArea>
+    <style.NavBody>
+      <style.LogoArea>
         <Image
           src="/icons/logo.svg"
           width={153}
           height={40}
           layout="responsive"
         />
-      </LogoArea>
-      <MenuList>
-        <MenuLinks>
-          <MenuItem>
-            <MenuText>가입하기</MenuText>
-          </MenuItem>
-          <MenuItem>
-            <MenuText>|</MenuText>
-          </MenuItem>
-          <MenuItem>
-            <MenuText>로그인</MenuText>
-          </MenuItem>
-          <Hambuger>
+      </style.LogoArea>
+      <style.MenuList>
+        <style.MenuLinks>
+          <style.MenuItem>
+            <style.MenuText>가입하기</style.MenuText>
+          </style.MenuItem>
+          <style.MenuItem>
+            <style.MenuText>|</style.MenuText>
+          </style.MenuItem>
+          <style.MenuItem onClick={redirectToSpotifyAuth}>
+            <style.MenuText>로그인</style.MenuText>
+          </style.MenuItem>
+          <style.Hambuger>
             <Image
               src="/icons/menu.svg"
               width={30}
               height={30}
               layout="responsive"
             />
-          </Hambuger>
-        </MenuLinks>
-      </MenuList>
-    </NavBody>
+          </style.Hambuger>
+        </style.MenuLinks>
+      </style.MenuList>
+    </style.NavBody>
   );
 };
 

85dcab3269a4e96c3b958528d88beb274e8c52ea
RookieAND <gwangin1999@naver.com>
2023-02-27 17:39:53 +0900

    feat: remove unused next page component
    더 이상 사용하지 않는 페이지 및 임시로 추가했던 버튼 등을 삭제했습니다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
 import Head from 'next/head';
 
-import { redirectToSpotifyAuth } from '@/apis/spotify';
-
 const Home = () => {
   return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#00917C" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>MZ2MO : 음악의 세계를 열다</title>
-      </Head>
-      <button onClick={redirectToSpotifyAuth}>Login</button>
-    </>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#00917C" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>MZ2MO : 음악의 세계를 열다</title>
+    </Head>
   );
 };
 

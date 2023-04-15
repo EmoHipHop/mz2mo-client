2c1425277928db08d5d064c9acfaf16f54092845
yuncs <changsoonyun@naver.com>
2023-04-15 17:40:18 +0900

    chore: 공유되지 않아서 임시 주석처리

--- src/constants/apis/server.ts
+++ src/constants/apis/server.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'server_domain:8080/api/v1' as const;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const SPOTIFY_API_URL = 'https://api.spotify.com/v1' as const;

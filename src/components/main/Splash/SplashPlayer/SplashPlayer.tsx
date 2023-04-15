de23871de6af7d9dd41c0bbc41943a04279b2830
yuncs <changsoonyun@naver.com>
2023-04-15 17:38:20 +0900

    feat: Splash player 추가

--- src/components/main/Splash/SplashPlayer/SplashPlayer.tsx
+++ src/components/main/Splash/SplashPlayer/SplashPlayer.tsx
@@ -3,30 +3,30 @@ import ForWardSvg from '@/assets/icons/images/forwardIcon.svg';
 import PlayPauseIconSvg from '@/assets/icons/images/playPauseIcon.svg';
 import * as style from './SplashPlayer.style';
 
-export default function SplashPlayer() {
-  return (
-    <>
-      <style.DiskPlayer>
-        <style.Disk />
-        <style.DiskController>
-          <style.BackWard>
-            <BackWardSvg width={20} height={20} />
-          </style.BackWard>
-          <style.PlayPause>
-            <PlayPauseIconSvg width={27} height={35} />
-          </style.PlayPause>
-          <style.Forward>
-            <ForWardSvg width={20} height={20} />
-          </style.Forward>
-        </style.DiskController>
-      </style.DiskPlayer>
-      <style.SongInfo>
-        <style.SongTitle>LoveDive</style.SongTitle>
-        <style.SongSinger>IVE</style.SongSinger>
-      </style.SongInfo>
-      <style.ProgressBar>
-        <input type="range" />
-      </style.ProgressBar>
-    </>
-  );
-}
+const SplashPlayer = () => (
+  <>
+    <style.DiskPlayer>
+      <style.Disk />
+      <style.DiskController>
+        <style.BackWard>
+          <BackWardSvg width={20} height={20} />
+        </style.BackWard>
+        <style.PlayPause>
+          <PlayPauseIconSvg width={27} height={35} />
+        </style.PlayPause>
+        <style.Forward>
+          <ForWardSvg width={20} height={20} />
+        </style.Forward>
+      </style.DiskController>
+    </style.DiskPlayer>
+    <style.SongInfo>
+      <style.SongTitle>LoveDive</style.SongTitle>
+      <style.SongSinger>IVE</style.SongSinger>
+    </style.SongInfo>
+    <style.ProgressBar>
+      <input type="range" />
+    </style.ProgressBar>
+  </>
+);
+
+export default SplashPlayer;
